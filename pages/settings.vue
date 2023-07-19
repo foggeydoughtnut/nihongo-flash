f0cf4be6551a8909f91b790f0746a73ea41375fb
Jeff Anderson <foggeydoughtnut@gmail.com>
2023-07-18 21:37:59 -0600

    basic layout of settings page"

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>
-    This is the settings page
+  <div class="font-sans flex flex-col md:w-3/4 w-11/12 mx-auto h-[80vh]">
+    <h1 class="font-semibold text-4xl mt-8 text-center flex-1">Settings</h1>
+    <NuxtLink
+      to="/login"
+      class="self-center text-center text-xl p-4 px-8 rounded-xl mt-14 font-semibold shadow-md hover:shadow-lg delay-75 duration-300 transition-all bg-red-300 hover:bg-red-400"
+    >
+      Logout
+    </NuxtLink>
   </div>
 </template>
 
 <script lang="ts">
+import { NuxtLink } from '.nuxt/components';
+
   definePageMeta({
     layout: 'navbar-layout',
   })
